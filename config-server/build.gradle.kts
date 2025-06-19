5013507d29abe0a2cfcd796ed8a889121a334c0e
HyeonGi Min <hyeoni1995@gmail.com>
2025-06-19 21:33:30 +0900

    WIS-5 Add Spring Cloud Config client setup and initial application files

--- config-server/build.gradle.kts
+++ config-server/build.gradle.kts
@@ -1,12 +1,14 @@
 plugins {
     id("org.springframework.boot") version "3.2.5"
     id("io.spring.dependency-management") version "1.1.4"
-    id("java")
+    java
 }
 
+group = "com.wimp"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_17
+
 repositories {
-    group = "com.wimp"
-    version = "0.0.1-SNAPSHOT"
     mavenCentral()
 }
 
@@ -14,10 +16,6 @@ dependencies {
     implementation("org.springframework.cloud:spring-cloud-config-server")
     implementation("org.springframework.boot:spring-boot-starter-web")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-}
-
-tasks.test {
-    useJUnitPlatform()
 }
 
 dependencyManagement {
---
+++ config-test/build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    id("org.springframework.boot") version "3.2.5"
+    id("io.spring.dependency-management") version "1.1.4"
+    java
+}
+
+group = "com.wimp"
+version = "0.0.1-SNAPSHOT"
+
+java.sourceCompatibility = JavaVersion.VERSION_17
+
+repositories {
+    mavenCentral()
+}
+
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:2023.0.1")
+    }
+}
+
+dependencies {
+    implementation("org.springframework.cloud:spring-cloud-starter-config") // ✅ Spring Cloud Client
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
